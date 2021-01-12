03cb1975ea9b4fd8308b6bba5edf79b3db22402c
Kyoko Caraway <kcaraway@et-c6.kzn.21-school.ru>
2021-01-12 06:02:41 +0300

    начал хоть как-то работать philo_one

--- philo_one/main.c
+++ philo_one/main.c
@@ -27,9 +27,124 @@ int	parse_argv(int ac, char **av)
 	return 0;
 }
 
+
+size_t	get_time(struct timeval t1)
+{
+	struct timeval t2;
+
+	gettimeofday(&t2, NULL);
+	return ((t2.tv_sec - t1.tv_sec) * 1000 + (t2.tv_usec - t1.tv_usec) / 1000);
+}
+
+void	function_death_print(t_philo *tmp)
+{
+	pthread_mutex_unlock(&tmp->death);
+	printf("%lu %lu died\n", get_time(tmp->start_time), tmp->number);
+}
+
+
+void	*death(void *one)
+{
+	t_philo		*tmp;
+	size_t		diff_time;
+
+	tmp = one;
+	while (tmp->life == 1)
+	{
+		if (tmp->time_of_life < (diff_time = get_time(tmp->start_time)))
+			function_death_print(tmp);
+		usleep (50);
+	}
+}
+
+void	*stop(void *two)
+{
+	t_philo		*tmp;
+
+	tmp = two;
+	pthread_mutex_lock(&tmp->death);
+	tmp->life = 0;
+	pthread_mutex_unlock(&tmp->death);
+	return 0;
+}
+
+void	function_eating(t_philo *tmp)
+{
+	struct timeval	start_eating;
+
+	pthread_mutex_lock(&tmp->fork[tmp->number - 1]);
+	printf("%lu %lu has taken a fork\n", get_time(tmp->start_time), tmp->number);
+	if (tmp->number != g_data.quantity_philo)
+		pthread_mutex_lock(&tmp->fork[tmp->number]);
+	else
+		pthread_mutex_lock(&tmp->fork[0]);
+	gettimeofday(&start_eating, NULL);
+	printf("%lu %lu is eating\n", get_time(tmp->start_time), tmp->number);
+	while (g_data.time_to_eat > get_time(start_eating))
+		usleep(50);
+	pthread_mutex_unlock(&tmp->fork[tmp->number]);
+	if (tmp->number != g_data.quantity_philo)
+		pthread_mutex_unlock(&tmp->fork[tmp->number]);
+	else
+		pthread_mutex_unlock(&tmp->fork[0]);
+}
+
+void	function_sleep(t_philo *tmp)
+{
+	struct timeval	start_sleep;
+
+	gettimeofday(&start_sleep, NULL);
+	printf("%lu %lu is sleeping\n", get_time(tmp->start_time), tmp->number);
+	while (g_data.time_to_sleep > get_time(start_sleep))
+		usleep(50);
+}
+
+void	function_think(t_philo *tmp)
+{
+	printf("%lu %lu is thinking\n", get_time(tmp->start_time), tmp->number);
+	
+}
+
+void	*function_philo_one(void *star)
+{
+	t_philo		*tmp;
+	pthread_t	one;
+	pthread_t	two;
+	tmp = star;
+
+	pthread_create(&one, NULL, death, tmp);
+	pthread_create(&two, NULL, stop, tmp);
+
+	while (tmp->life == 1)
+	{
+		function_eating(tmp);
+		function_sleep(tmp);
+		function_think(tmp);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	if (parse_argv(ac, av) == -1)
+	size_t 	i;
+	t_philo *tmp;
+
+	i = 0;
+	if (parse_argv(ac, av) == -1)//tolbko cufri
 		return -1;
+	g_data.philo = malloc(g_data.quantity_philo * sizeof(t_philo));
+	tmp = g_data.philo;
+	g_data.philo->fork = malloc(g_data.quantity_philo * sizeof(pthread_mutex_t));
+	g_data.philo->thread = malloc(g_data.quantity_philo * sizeof(pthread_t));
+	pthread_mutex_init(g_data.philo->fork, NULL);
+	while (i < g_data.quantity_philo)
+	{
+		gettimeofday(&tmp[i].start_time, NULL);
+		tmp->number = i + 1;
+		tmp[i].life = 1;
+		pthread_create(g_data.philo->thread, NULL, function_philo_one, &tmp[i]);
+		pthread_join(*g_data.philo->thread, NULL);
+		++i;
+	}
+
 	return 0;
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -1,20 +1,31 @@
 
 # include <unistd.h>
 # include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdio.h>
 
 typedef struct	s_philo_one
 {
-	pthread_mutex_t v;
-	pthread_t		c;
+	pthread_mutex_t *fork;
+	pthread_mutex_t print;
+	pthread_mutex_t death;
+	
+	pthread_t		*thread;
+	int				life;
+	struct timeval	start_time;
+	size_t			time_of_life;
+	size_t			number;
+
 }				t_philo;
 
 typedef struct	s_data
 {
-	int		quantity_philo;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		number_of_eat;
+	size_t	quantity_philo;
+	size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+	size_t	number_of_eat;
 	t_philo	*philo;
 
 }				t_data;
